d0e38ba4c58b5058f822d52e7a86188efa434597
Abeer Shaikh <shaikhabeer4@gmail.com>
2024-04-02 08:35:04 +0500

    Typescript Assignment

--- main.ts
+++ main.ts
@@ -532,9 +532,3 @@ function show_magicians(magicians: string[]){
     }
 }
 show_magicians(magicianNames)
-
-// Exercise 42 Great Magicians: Start with a copy of your program from Exercise 41. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
-function make_great(message:string){
-    let message= 'Great'
-    }
-}
